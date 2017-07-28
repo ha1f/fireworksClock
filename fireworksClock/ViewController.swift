8e4495d01cb4b203dcf69941575841540a8e7d9a
ha1f <half.ldk.hiz@gmail.com>
2017-07-28 22:29:37 +0900

    一旦時計にした

--- fireworksClock/ViewController.swift
+++ fireworksClock/ViewController.swift
@@ -8,18 +8,50 @@
 
 import UIKit
 
+class FireworksView: UIView {
+    
+    func configure() {
+        let layer = CAEmitterLayer()
+        
+        
+        
+        self.layer.addSublayer(layer)
+    }
+    
+}
+
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var fireworksView: FireworksView!
+    @IBOutlet weak var timeLabel: UILabel!
+    
+    var updateTimer: Timer!
+    lazy var dateFormatter: DateFormatter = {
+        var formatter = DateFormatter()
+        formatter.dateStyle = .none
+        formatter.timeStyle = .short
+        return formatter
+    }()
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        UIApplication.shared.isIdleTimerDisabled = true
+        
+        updateTimer = Timer(timeInterval: 0.5, repeats: true) { [weak self] _ in
+            guard let unwrappedSelf = self else {
+                return
+            }
+            let now = Date()
+            unwrappedSelf.timeLabel.text = unwrappedSelf.dateFormatter.string(from: now)
+        }
+        updateTimer.fire()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        UIApplication.shared.isIdleTimerDisabled = false
+        updateTimer.invalidate()
     }
-
 
 }
 
